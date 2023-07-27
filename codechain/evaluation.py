dc64883597b2481716451b2c99de203417b2315a
James Murdza <james@jamesmurdza.com>
2023-07-27 12:27:14 -0400

    Make problems an instance variable.

--- codechain/evaluation.py
+++ codechain/evaluation.py
@@ -6,14 +6,15 @@ from langsmith.schemas import Run, Example
 from human_eval.execution import check_correctness
 from human_eval.data import read_problems
 
-problems = read_problems()
-
 class HumanEvalEvaluator(RunEvaluator):
     """ This is a wrapper around the HumanEval evaluator. """
 
+    def __init__(self):
+        self.problems = read_problems()
+
     def evaluate_run(self, run: Run, example: Optional[Example] = None) -> EvaluationResult:
         print("Evaluating " + run.inputs["task_id"])
-        problem = problems[run.inputs["task_id"]]
+        problem = HumanEvalEvaluator.self.problems[run.inputs["task_id"]]
         solution = run.outputs["output"]
 
         # The HumanEval evaluator, which runs the Python code against unit tests
